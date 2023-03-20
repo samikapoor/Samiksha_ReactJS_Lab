103e05b5ff0b946ce31ad7ff60748cb9e5347bf1
samikapoor <kapoormisha52@gmail.com>
2023-03-20 21:02:16 +0530

    first push

---
+++ expense-tracker/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import  {createRoot} from 'react-dom/client';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// const root =createRoot(document.getElementById("root") as HTMLElement);
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
